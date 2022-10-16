ed1e5c82bc4ae2b71bf74d1e34e795157e1cf47f
Daniel Silveira <murtdaniel@gmail.com>
2022-10-16 19:18:01 -0300

    Projeto em estado inicial. Jogo da advinhação (1.0)

--- Ex01.py
+++ Ex01.py
@@ -1,1 +1,13 @@
-print('Bem vindo ao GitHub')
+from random import randint
+from time import sleep
+computador = randint(0, 5)
+print('_' *50)
+print('Vou pensar em um número de 0 à 5 tente advinhar...')
+print('_' *50)
+N1 = int(input('Em que número eu pensei?: '))
+print('PROCESSANDO...')
+sleep(2)
+if N1 == computador:
+    print("Acertou!! Eu pensei no número {}".format(computador))
+else:
+    print('Você Perdeu!! Eu pensei no número {}'.format(computador))
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Primeiro projeto dentro do GitHub
+
+Jogo da Adivinhação!
+
+O programa vai escolher um número entre 0 e 5. Logo após
+você poderá digitar um número e tentar acertar qual foi escolhido 
+pelo programa anteriormente. Caso acerte, você ganhará o jogo, caso erre, o programa
+ganhará!
